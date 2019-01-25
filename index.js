948fa5daa8f13897e70ed4116f9285eec2f08bed
Edgar Rolando Cosajay Campos <ondasycircuitos@gmail.com>
2019-01-25 01:43:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,7 +54,10 @@ function total() {
 function removeFromCart(item) {
    for( var i = 0; i < cart.length-1; i++){
    if ( cart[i].itemName === item) {
-     cart.splice(i, 1); 
+     cart.splice(i, 1);
+   }
+   else{
+    return "That item is not in your cart.";
    }
 }
 
