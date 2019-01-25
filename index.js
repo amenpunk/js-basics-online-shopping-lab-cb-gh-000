9de231be80ceb890b1710d4b64d76531251028a3
Edgar Rolando Cosajay Campos <ondasycircuitos@gmail.com>
2019-01-25 01:59:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,20 +50,17 @@ function total() {
   }
   return total;
 }
-
 function removeFromCart(item) {
-    var alerts =  "That item is not in your cart."
-    console.log("item to remove: " + item)
-   for( var i = 1; i < cart.length; i++){
-   if ( cart[i].itemName === item) {
-    console.log("index item" + cart[i].itemName)
-     var nuevo = cart.splice(i, 1);
-     return nuevo;
-   }
-   else{
-    return alerts;
-   }
-
+  // write your code here
+  let itemsInCart = false;
+    if (!itemsInCart) {
+      return `That item is not in your cart.`;
+    } 
+    const valueToRemove = item;
+    const filteredItems = cart.filter(function(x) {
+    return x !== valueToRemove;
+} 
+}
 }
 
 }
